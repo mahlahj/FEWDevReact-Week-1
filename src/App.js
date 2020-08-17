d78617d720ed68f68913e069b00caf2051b5a302
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-08-17 04:20:48 -0500

    ReactComponentsOne

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 /* Import own things. */
 import { Navbar, NavbarBrand } from 'reactstrap';
+/* Import the menu from MenuComponents.js */
+import Menu from './components/MenuComponents';
 import './App.css';
+
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }

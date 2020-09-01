edbb8a755e6b928c822240e7ad53ec1c99b5cc48
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-09-01 00:42:16 -0500

    SinglePageApplicationPart2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,44 +2,35 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-    function RenderDish( {dish} ) {
+function RenderDish( {dish} ) {
+    return (
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+function RenderComments( {comments} ) {
+    if (comments != null)
         return (
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-    }
-
-    function RenderComments( {comments} ) {
-        if (array.length != 0) {
-            return (
-                <div></div>
-            );
-        }
-
-        const renderedComments = comments.map((comment) => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>{comment.author}, {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'short',
-                        day: '2-digit'}).format(new Data(Date.parse(comment.date)))}</p>
-                </li>
-            );
-        });
-
-        return (
-            <div>
-                <h4>
-                    Comments
-                </h4>
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    { renderedComments }
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {
+                                    year: 'numeric',
+                                    month: 'short',
+                                    day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         );
@@ -69,12 +60,10 @@ import { Link } from 'react-router-dom';
                     </div>
                 </div>
             );
-        }
-        else {
+            else
             return(
                 <div></div>
-            );
-        }
+                );
     }
 
 export default DishDetail;

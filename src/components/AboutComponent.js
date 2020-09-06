b477b047d9d1e742eab9434d3e63444aba7f0202
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-09-05 20:04:55 -0500

    ControlledForms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function RenderLeader ({leader}) {
+    return (
+        <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={leader.image} alt={leader.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.description}</p>
+                  </Media>
+                </Media>
+              </div>
+    );
+}
+
 function About(props) {
+
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <RenderLeader leader = {leader}/>
         );
     });
 
@@ -25,10 +42,10 @@ function About(props) {
                 <div className="col-12 col-md-6">
                     <h2>Our History</h2>
                     <p>
-                        Started in 2010, Ristorante con Fusion quickly established itself as
-                        a culinary icon par excellence in Hong Kong.
-                        With its unique brand of world fusion cuisine that can be found
-                        nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.
+                        Started in 2010, Ristorante con Fusion quickly established itself
+                        as a culinary icon par excellence in Hong Kong.
+                        With its unique brand of world fusion cuisine that can be found nowhere
+                        else, it enjoys patronage from the A-list clientele in Hong Kong.
                         Featuring four of the best three-star Michelin chefs in the world,
                         you never know what will arrive on your plate the next time you visit us.
                     </p>
@@ -84,4 +101,4 @@ function About(props) {
     );
 }
 
-export default About;    
+export default About;

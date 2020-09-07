8195dd96bcf21d5d162f80d1f808dabcb6d94cbd
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-09-06 21:35:33 -0500

    IntroToRedux

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ Import the menu from MenuComponents.js
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 
 class App extends Component {
   /*Remove the constructor completely after the MainComponent is ready.
@@ -23,22 +26,24 @@ class App extends Component {
 
   render () {
     return (
-      <BrowserRouter>
-        <div>
-          {/* 10.- The nav bar was moved to the MainComponents, so can be deleted.
-            After this only the Main is rendered.
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes={this.state.dishes}/> 8.- dishes equal to this.state.dishes.
-                                               The dishes defined in the state for the App 
-                                               component are available as props to the menu
-                                               component.*/ }
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store = {store}>
+        <BrowserRouter>
+          <div>
+            {/* 10.- The nav bar was moved to the MainComponents, so can be deleted.
+              After this only the Main is rendered.
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+              </div>
+            </Navbar>
+            <Menu dishes={this.state.dishes}/> 8.- dishes equal to this.state.dishes.
+                                                The dishes defined in the state for the App 
+                                                component are available as props to the menu
+                                                component.*/ }
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
